459306fe1bc1bf5b9bbb05dfff26abffe24aaac6
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-04-14 09:38:43 +0300

    Removed Yellow lines with Expanded widgets

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -29,14 +29,16 @@ class ResultsScreen extends StatelessWidget {
         width: double.infinity,
         child: Container(
           margin: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("You answered x out of y questions correctly"),
-              const SizedBox(height: 30),
-              QuestionsSummary(getSummaryData()),
-              TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
-            ],
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("You answered x out of y questions correctly"),
+                const SizedBox(height: 30),
+                QuestionsSummary(getSummaryData()),
+                TextButton(onPressed: () {}, child: const Text('Restart Quiz'))
+              ],
+            ),
           ),
         ));
   }
